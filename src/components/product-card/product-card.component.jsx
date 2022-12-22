58549a830efa2da7b85d92fa747adce6ab0f18a3
bjweidongjun <bjweidongjun@corp.netease.com>
2022-12-22 14:42:32 +0800

    added checkout

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -14,8 +14,8 @@ const ProductCard = ({ product }) => {
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
       <div className="footer">
-        <span className={name}></span>
-        <span className={price}></span>
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
       </div>
       <Button buttonType={"inverted"} onClick={addProductToCart}>
         Add to cart
