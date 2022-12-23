8e9ce30a6202cc97311911a0566713c87f59f6fb
bjweidongjun <bjweidongjun@corp.netease.com>
2022-12-23 20:27:02 +0800

    added redux for cart

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,9 @@
-import { Fragment, useContext } from "react";
+import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
-import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import "./navigation.styles.jsx";
@@ -15,10 +14,11 @@ import {
   NavLink,
 } from "./navigation.styles.jsx";
 import { selectCurrentUser } from "../../store/user/user.selector";
+import { selectIsCartOpen } from "../../store/cart/cart.selector";
 
 const Navigation = () => {
   const currentUser = useSelector(selectCurrentUser);
-  const { isCartOpen } = useContext(CartContext);
+  const isCartOpen = useSelector(selectIsCartOpen);
 
   return (
     <Fragment>
---
+++ src/store/cart/cart.action.js
@@ -0,0 +1,61 @@
+import { createAction } from "../../utils/reducer/reducer.utils";
+import { CART_ACTION_TYPE } from "./cart.types";
+
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
+const clearCartItem = (cartItems, cartItemToClear) =>
+  cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
+export const setIsCartOpen = (isCartOpen) =>
+  createAction(CART_ACTION_TYPE.SET_IS_CART_OPEN, isCartOpen);
+
+export const addItemToCart = (cartItems, productToAdd) => {
+  console.log("addItemToCart", cartItems, productToAdd);
+  return createAction(
+    CART_ACTION_TYPE.SET_CART_ITEMS,
+    addCartItem(cartItems, productToAdd)
+  );
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  console.log("removeItemFromCart", cartItemToRemove);
+  return createAction(
+    CART_ACTION_TYPE.SET_CART_ITEMS,
+    removeCartItem(cartItems, cartItemToRemove)
+  );
+};
+
+export const clearItemFromCart = (cartItems, cartItemToClear) => {
+  console.log("clearItemFromCart", cartItemToClear);
+  return createAction(
+    CART_ACTION_TYPE.SET_CART_ITEMS,
+    clearCartItem(cartItems, cartItemToClear)
+  );
+};
